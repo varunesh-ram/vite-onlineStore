2c17c5f78fe1d3bc015ef9b30832b9021643f36c
varunesh-ram <varuneshram@yahoo.com>
2024-12-08 02:59:03 +0100

    chore : Initial frontend build steup

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,14 @@
-import { useState } from 'react'
-import './App.css'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./container/login/index";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
---
+++ src/container/login/index.js
@@ -0,0 +1,13 @@
+const Login = () => {
+
+    return (
+       <div className="login-container">
+         <div className="login-form">
+           <h1>Login</h1>
+         </div>
+       </div>
+    );
+   };
+   
+   
+   export default Login;
